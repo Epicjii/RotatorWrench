e3b78bf5d9dd71df237a6666b30a902a4a9b2814
Jacob Liggett <jacobliggett2000@gmail.com>
2023-09-02 17:09:12 -0400

    Added New Features
    -Now checks if you already have one.
    -Now puts the wrench directly into your hand.

--- src/main/kotlin/rotatorwrench/RotatorWrenchSummon.kt
+++ src/main/kotlin/rotatorwrench/RotatorWrenchSummon.kt
@@ -1,5 +1,6 @@
 package rotatorwrench
 
+import net.kyori.adventure.text.Component
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -12,7 +13,16 @@ class RotatorWrenchSummon : CommandExecutor {
             return false
         }
         val wrenchItem = RotatorWrenchItem().createWrench()
-        sender.inventory.addItem(wrenchItem)
+        if (!sender.inventory.contains(wrenchItem)) {
+            sender.inventory.addItem(wrenchItem)
+        }
+        val wrenchIndex = sender.inventory.first(wrenchItem)
+        if (wrenchIndex == -1) {
+            sender.sendMessage(Component.text("Hey! Your Inventory is Full!"))
+            return true
+        }
+        sender.inventory.setItem(wrenchIndex, sender.inventory.itemInMainHand)
+        sender.inventory.setItemInMainHand(wrenchItem)
 
         return true
     }
